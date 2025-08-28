3b4450aa803fc6377c91cf9a2b8727b004961731
Nandar <nandar@mloptic.com>
2025-08-28 16:47:26 +0700

    implement dependency injection

---
+++ lib/init_dependencies.dart
@@ -0,0 +1,32 @@
+import 'package:blog_app/features/auth/data/data_sources/auth_remote_data_source.dart';
+import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
+import 'package:blog_app/features/auth/domain/repositories/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/register.dart';
+import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:get_it/get_it.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final serviceLocator = GetIt.instance;
+
+Future<void> initDependencies() async {
+  _initAuth();
+  final supabase = await Supabase.initialize(
+    url: dotenv.env['SUPABASE_PROJECT_URL'] ?? '',
+    anonKey: dotenv.env['SUPABASE_API_KEY'] ?? '',
+  );
+  serviceLocator.registerLazySingleton(() => supabase.client);
+}
+
+void _initAuth() {
+  serviceLocator.registerFactory<AuthRemoteDataSource>(
+    () => AuthRemoteDataSourceImpl(serviceLocator()),
+  );
+  serviceLocator.registerFactory<AuthRepository>(
+    () => AuthRepositoryImpl(serviceLocator()),
+  );
+  serviceLocator.registerFactory(() => Register(serviceLocator()));
+  serviceLocator.registerLazySingleton(
+    () => AuthBloc(register: serviceLocator()),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,19 @@
 import 'package:blog_app/core/theme/app_theme.dart';
-import 'package:blog_app/features/auth/data/data_sources/auth_remote_data_source.dart';
-import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
-import 'package:blog_app/features/auth/domain/usecases/register.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
+import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  final supabase = await Supabase.initialize(
-    url: dotenv.env['SUPABASE_PROJECT_URL'] ?? '',
-    anonKey: dotenv.env['SUPABASE_API_KEY'] ?? '',
-  );
+  await initDependencies();
+
   runApp(
     MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => AuthBloc(
-            register: Register(
-              AuthRepositoryImpl(AuthRemoteDataSourceImpl(supabase.client)),
-            ),
-          ),
-        ),
-      ],
+      providers: [BlocProvider(create: (_) => serviceLocator<AuthBloc>())],
       child: const MyApp(),
     ),
   );
